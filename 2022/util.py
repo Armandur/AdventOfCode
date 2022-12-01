85e9a23cd684b2c9b52489d04c6fc9d8f86854aa
Rasmus Pettersson Vik <rasmus.pettersson.vik@gmail.com>
2022-12-01 11:20:03 +0100

    Updated GET and POST to comply with user info.

--- 2022/util.py
+++ 2022/util.py
@@ -2,13 +2,15 @@ import requests, os
 
 baseURL = "https://adventofcode.com/"
 
+headers = {"User-Agent": "github.com/Armandur/AdventOfCode by rasmus.pettersson.vik@gmail.com"}
+
 def getInput(year, day, cookie):
     fileName = f"{year}/{year}-{day}.txt"
     if os.path.isfile(fileName):
         with open(fileName, 'r') as file:
             return file.read().splitlines()
 
-    request = requests.get(f"{baseURL}{year}/day/{day}/input", cookies={"session": cookie})
+    request = requests.get(f"{baseURL}{year}/day/{day}/input", cookies={"session": cookie}, headers=headers)
 
     if request.status_code == 200:
         with open(fileName, 'w') as file:
@@ -19,7 +21,7 @@ def getInput(year, day, cookie):
 
 def postAnswer(year, day, level, answer, cookie):
     data = {"answer": answer, "level": level}
-    request = requests.post(f"{baseURL}{year}/day/{day}/answer", data, cookies={"session": cookie})
+    request = requests.post(f"{baseURL}{year}/day/{day}/answer", data, cookies={"session": cookie}, headers=headers)
 
     for line in request.text.splitlines():
         if line.startswith("<article"):
