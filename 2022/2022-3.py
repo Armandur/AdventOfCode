a6d7d3a1817a0388f2eec6bde6b353d5363faa84
Rasmus Pettersson Vik <rasmus.pettersson.vik@gmail.com>
2022-12-03 10:51:47 +0100

    Third day done!

---
+++ 2022/2022-3.py
@@ -0,0 +1,72 @@
+import util
+import datetime
+import sys
+
+
+lowerDelta = 96
+upperDelta = 38
+
+
+def part1(input):
+	count = 0
+
+	for line in input:
+		container1 = line[:len(line)//2]
+		container2 = line[len(line)//2:]
+		
+		container1 = [*container1]
+		container2 = [*container2]
+
+		common = ''.join(set(container1) & set(container2))
+		
+		priority = 0
+		if common.islower():
+			priority = ord(common)-lowerDelta
+		else:
+			priority = ord(common)-upperDelta
+		
+		#print(f"{common} - {priority}")
+		count += priority
+		
+	return count
+
+
+def part2(input):
+	count = 0
+
+	group = []
+	for num, line in enumerate(input):
+		if num % 3 == 2: #Third line in group
+			group.append([*line])
+
+			common = ''.join(set(group[0]) & set(group[1]) & set(group[2]))
+			
+			priority = 0
+			if common.islower():
+				priority = ord(common)-lowerDelta
+			else:
+				priority = ord(common)-upperDelta
+		
+			#print(f"{common} - {priority}")
+			count += priority		
+
+			group = []
+		else:
+			group.append(line)	
+
+	return count
+
+
+if __name__ == '__main__':
+	cookie = sys.argv[1]
+	today = datetime.datetime.now()
+	input = util.getInput(today.year, today.day, cookie)
+	test = []
+	with open(f"{today.year}/test.txt", "r") as file:
+		test = file.read().splitlines()
+
+	print(f"Part one: {part1(input)}")
+	#print(util.postAnswer(today.year, today.day, 1, part1(input), cookie))
+
+	print(f"Part two: {part2(input)}")
+	#print(util.postAnswer(today.year, today.day, 2, part2(input), cookie))
--- 2022/template.py
+++ 2022/template.py
@@ -18,9 +18,9 @@ def part2(input):
 if __name__ == '__main__':
 	cookie = sys.argv[1]
 	today = datetime.datetime.now()
-	input = util.getInput(today.year, today.da, cookie)
+	input = util.getInput(today.year, today.day, cookie)
 	test = []
-	with open("test.txt", "r") as file:
+	with open(f"{today.year}/test.txt", "r") as file:
 		test = file.read().splitlines()
 
 	print(f"Part one: {part1(input)}")
