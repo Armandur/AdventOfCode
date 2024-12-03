08b66dc18fc967c1f06ac3210a327b894e1bfc00
Rasmus Pettersson Vik <rasmus.pettersson.vik@gmail.com>
2024-12-03 12:53:50 +0100

    part2 done!

--- 2024/2024-3.py
+++ 2024/2024-3.py
@@ -65,9 +65,51 @@ def part1(input):
 
 def part2(input):
 	count = 0
-	
-	return count
+	program = []
+	for line in input:
+		index = line.find("mul(")
+		positions = []
+		operations = []
+		while index != -1:
+			positions.append(index)
+			index = line.find("mul(", index+len("mul("))
+		for position in positions:
+			endParenthesis = line.find(")", position+len("mul(")+3, position+len("mul(")+8)
+			if endParenthesis != -1:				
+				operation = line[position+len("mul("):endParenthesis]
+				try:
+					if len(operation.split(',')) > 1:
+						if " " not in operation.split(',')[0] and " " not in operation.split(',')[1]:
+							operations.append((position, [int(x) for x in operation.split(',')]))
+				except ValueError:
+					pass
 
+		# Part 2 All mul() operations found, search for all do() and don't()
+		index = line.find("do()")
+		while index != -1:
+			operations.append((index, ("do()")))
+			index = line.find("do()", index+len("do()"))
+		
+		index = line.find("don't()")
+		while index != -1:
+			operations.append((index, ("don't()")))
+			index = line.find("don't()", index+len("don't()"))
+		program.append(operations)
+
+	for line in program:
+		line.sort(key=lambda x: x[0])
+	
+	do = True
+	for line in program:
+		for operation in line:
+			if operation[1] == "do()":
+				do = True
+			elif operation[1] == "don't()":
+				do = False
+			else:
+				if do:
+					count += operation[1][0] * operation[1][1]
+	return count
 
 if __name__ == '__main__':
 	cookie = sys.argv[1]
@@ -78,7 +120,7 @@ if __name__ == '__main__':
 		test = file.read().splitlines()
 
 	print(f"Part one: {part1(input)}")
-	print(util.postAnswer(today.year, today.day, 1, part1(input), cookie))
+	#print(util.postAnswer(today.year, today.day, 1, part1(input), cookie))
 
 	print(f"Part two: {part2(input)}")
 	#print(util.postAnswer(today.year, today.day, 2, part2(input), cookie))
