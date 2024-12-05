78d4b89f3f1290e017b5534cb147f83bb9963f7b
Rasmus Pettersson Vik <rasmus.pettersson.vik@gmail.com>
2024-12-05 19:32:06 +0100

    Day 4, part 2 done!

--- 2024/2024-4.py
+++ 2024/2024-4.py
@@ -5,6 +5,18 @@ from pprint import pprint
 import time
 import os
 
+def printWordmap(wordMap, marked=list([]), color=util.colors.green):
+	mapWidth = len(wordMap[0])
+	mapHeight = len(wordMap)
+	for y in range(0, mapHeight):
+		line = ""
+		for x in range(0, mapWidth):
+			if marked and any((x, y) in sublist for sublist in marked):
+				line += util.colorString(wordMap[y][x], color)
+			else:
+				line += wordMap[y][x]
+		print(line)
+
 def part1(puzzleInput):
 	count = 0
 
@@ -22,18 +34,6 @@ def part1(puzzleInput):
 	# 7 SAXAMASAAA
 	# 8 MAMMMXMMMM
 	# 9 MXMXAXMASX
-
-	def printWordmap(wordMap, marked=list([]), color=util.colors.green):
-		mapWidth = len(wordMap[0])
-		mapHeight = len(wordMap)
-		for y in range(0, mapHeight):
-			line = ""
-			for x in range(0, mapWidth):
-				if marked and any((x, y) in sublist for sublist in marked):
-					line += util.colorString(wordMap[y][x], color)
-				else:
-					line += wordMap[y][x]
-			print(line)
 
 	coordsXMAS = []
 
@@ -83,7 +83,6 @@ def part1(puzzleInput):
 		line = ""
 		for x in range(0, mapWidth):
 			if puzzleInput[y][x] == "X":
-				sumOfXMASCoords = len(coordsXMAS)
 				checkXMAS(puzzleInput, (x, y))
 					
 	#printWordmap(puzzleInput, coordsXMAS)
@@ -93,7 +92,59 @@ def part1(puzzleInput):
 
 def part2(puzzleInput):
 	count = 0
-	
+
+	MASCoords = []
+
+	def checkMAS(wordMap, pos):
+		posX = pos[0]
+		posY = pos[1]
+
+		def onBoard(pos):
+			x = pos[0]
+			y = pos[1]
+			if 0 <= x < len(wordMap[0]) and 0 <= y < len(wordMap):
+				return True
+			else:
+				return False
+			#       
+			#  -1 0 1 X
+			#-1 M   M
+			# 0   A 
+			# 1 S   S
+			# Y
+
+		deltas = [
+				[(-1, -1), (0, 0), (1, 1)],
+				[(1, -1), (0, 0), (-1, 1)]
+			]
+
+		MAS = ""
+		coords = []
+		for delta in deltas:
+			for i in range(3):
+				x = posX+delta[i][0]
+				y = posY+delta[i][1]
+				if onBoard((x, y)):
+					MAS += wordMap[y][x]
+					coords.append((x, y))
+				else:
+					MAS = ""
+					coords = []
+					break
+		
+		if ("MAS" in MAS[:3] or "MAS" in MAS[2::-1]) and ("MAS" in MAS[3:] or "MAS" in MAS[:-4:-1]):
+			MASCoords.append(coords)
+			#printWordmap(wordMap, MASCoords)
+			#print()
+
+	mapWidth = len(puzzleInput[0])
+	mapHeight = len(puzzleInput)
+	for y in range(0, mapHeight):
+		line = ""
+		for x in range(0, mapWidth):
+			if puzzleInput[y][x] == "A":
+				checkMAS(puzzleInput, (x, y))
+	count = len(MASCoords)
 	return count
 
 if __name__ == '__main__':
@@ -104,8 +155,8 @@ if __name__ == '__main__':
 	with open(f"{today.year}/test.txt", "r") as file:
 		test = file.read().splitlines()
 
-	print(f"Part one: {part1(puzzleInput)}")
-	print(util.postAnswer(today.year, 4, 1, part1(puzzleInput), cookie))
+	#print(f"Part one: {part1(puzzleInput)}")
+	#print(util.postAnswer(today.year, 4, 1, part1(puzzleInput), cookie))
 
-	print(f"Part two: {part2(puzzleInput)}")
-	#print(util.postAnswer(today.year, today.day, 2, part2(puzzleInput), cookie))
+	#print(f"Part two: {part2(puzzleInput)}")
+	#print(util.postAnswer(today.year, 4, 2, part2(puzzleInput), cookie))
